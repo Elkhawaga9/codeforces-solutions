440d3b02e30365c74c2c257c2ab502cee422895b
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-02-09 22:25:49 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/463B | Caisa and Pylons/220789690.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+#define elkhawaga  std::ios_base::sync_with_stdio(0)
+using namespace std;
+ 
+bool cmp(pair<string, ll>& a, pair<string, ll>& b)
+{
+    return  a.second == b.second ? a.first<b.first : a.second>b.second;
+}
+ 
+ 
+//vector<ll> getDivisors(int n)
+//{
+//    vector<ll>vec;
+//    // Note that this loop runs till square root
+//    for (int i = 1; i <= sqrt(n); i++)
+//    {
+//        if (n % i == 0)
+//        {
+//            // If divisors are equal, print only one
+//            if (n / i == i)
+//                vec.push_back(i);
+//
+//            else // Otherwise print both
+//            {
+//                vec.push_back(i);
+//                vec.push_back( n / i);
+//            }
+//        }
+//    }
+//    return vec;
+//}
+ 
+void tcase()
+{
+    int n;cin >> n;
+    //vector<ll>vec(n);
+    int temp1 = 0, temp2, ener = 0, sol = 0;
+    for (int i = 0;i < n;i++)
+    {
+        cin >> temp2;
+        ener += (temp1 - temp2);
+        if (ener < 0)
+        {
+            sol += (-1 * ener);
+            ener = 0;
+        }
+        temp1 = temp2;
+    }
+    cout << sol;
+}
+int main()
+{
+    elkhawaga;
+    ll t;
+    //cin >> t;
+    t = 1;
+    for (int i = 0;i < t;i++) tcase();
+}
