2807de1176f60f05f9bd780f3975fe4d565937fd
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-02-09 22:23:54 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1872B | The Corridor or There and Back Again/222292379.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+#define elkhawaga  std::ios_base::sync_with_stdio(0)
+using namespace std;
+ 
+bool cmp(pair<int,int>& a,pair<int, int>& b)
+{
+    return a.second < b.second;
+}
+ 
+ 
+//vector<ll> getDivisors(ll n)
+//{
+//    vector<ll>vec;
+//    // Note that this loop runs till square root
+//    for (ll i = 1; i <= sqrtl(n); i++)
+//    {
+//        if (n % i == 0)
+//        {
+//            // If divisors are equal, print only one
+//            if (n / i == i)
+//                vec.push_back(i);
+//
+//            else // Otherwise print both
+//            {
+//                vec.push_back(i);
+//                vec.push_back( n / i);
+//            }
+//        }
+//    }
+//    return vec;
+//}
+//bool if_prime(string n)
+//{
+//    int num = stoi(n);
+//    for (int i = 2;i * i <= num;++i) {
+//        if (num % i == 0) {
+//            return false;
+//        }
+//    }
+//    return 1;
+//}
+//vector<string>vec;
+//int freq[100005];
+void tcase()
+{
+    int n;cin >> n;
+    vector<ld>d(n), s(n);
+    ld ans = INT_MAX;
+    for (int i = 0;i < n;i++) cin >> d[i] >> s[i];
+    for (int i = 0;i < n;i++)
+    {
+        ans = min(ans, d[i] + ceil(s[i] / 2) - 1);
+    }
+    cout << ans << "\n";
+}                               
+int main()
+{
+    elkhawaga;
+    //cout << fixed << setprecision(10);
+    ll t;
+    cin >> t;
+   // t = 1;
+    for (int i = 0;i < t;i++) tcase();
+}
