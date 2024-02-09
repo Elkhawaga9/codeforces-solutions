673d106693cbb93bf9339521cf2cfd355f8226d3
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-02-09 22:25:26 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1287A | Angry Students/220949032.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+#define elkhawaga  std::ios_base::sync_with_stdio(0)
+using namespace std;
+ 
+bool cmp(pair<string, ll>& a, pair<string, ll>& b)
+{
+    return  a.second == b.second ? a.first<b.first : a.second>b.second;
+}
+ 
+ 
+//vector<ll> getDivisors(int n)
+//{
+//    vector<ll>vec;
+//    // Note that this loop runs till square root
+//    for (int i = 1; i <= sqrt(n); i++)
+//    {
+//        if (n % i == 0)
+//        {
+//            // If divisors are equal, print only one
+//            if (n / i == i)
+//                vec.push_back(i);
+//
+//            else // Otherwise print both
+//            {
+//                vec.push_back(i);
+//                vec.push_back( n / i);
+//            }
+//        }
+//    }
+//    return vec;
+//}
+ 
+void tcase()
+{
+    int n; cin >> n;
+    string s;cin >> s;
+    ll ans = 0;
+    for (int i = 0;i < n;i++)
+    {
+        ll temp_ans = 0;
+        if (s[i] == 'A')
+        {
+            for (int j = i + 1;j < n;j++)
+            {
+                if (s[j] == 'P') temp_ans++;
+                else break;
+            }
+        }
+        ans = max(ans, temp_ans);
+    }
+    cout << ans << "\n";
+}
+int main()
+{
+    elkhawaga;
+    ll t;
+    cin >> t;
+    //t = 1;
+    for (int i = 0;i < t;i++) tcase();
+}
