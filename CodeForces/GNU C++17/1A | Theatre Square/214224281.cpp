dbe8e820dfde573bec86544ac3ee987be1776f90
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-02-09 22:27:02 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1A | Theatre Square/214224281.cpp
@@ -0,0 +1,11 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+int main()
+{
+ unsigned long long n, m, a;
+ cin >> n >> m >> a;
+ unsigned long long val = ceil((double)m / a) * ceil((double)n / a);
+ cout << val;
+ return 0;
+}
