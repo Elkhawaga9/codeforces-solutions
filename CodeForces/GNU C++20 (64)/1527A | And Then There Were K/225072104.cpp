86ae98c562f8917923036aa5a79a731679bc3b26
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-02-09 22:22:27 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1527A | And Then There Were K/225072104.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ll;
+typedef long double ld;
+#define elkhawaga  std::ios_base::sync_with_stdio(0)
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n" 
+#define pi pair<int,int>
+using namespace std;
+const int N = 1e6 + 5;
+bool cmp(pair<int,int>& a,pair<int, int>& b)
+{
+    return a.first < b.first;
+}
+ 
+ 
+ 
+void tcase()
+{
+    int n;cin >> n;
+    int idx = 0;
+    for (int i = 0;i < 32;i++)
+    {
+        if (((1 << i) & n) == (1<<i))
+        {
+            idx = i;
+        }
+    }
+    cout << ((1 << (idx)) - 1);
+}
+int main()
+{
+    elkhawaga;
+    //cout << fixed << setprecision(10);
+    ll t;
+    cin >> t;
+   // t = 1;
+    for (int i = 0;i < t;i++)
+    {
+        tcase();
+        cout << "\n";
+    }
+}
