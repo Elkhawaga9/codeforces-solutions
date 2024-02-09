8b249b46625975c97a6c76feee7dcfd2a44f918e
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-02-09 22:25:40 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1294A | Collecting Coins/220791990.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+#define elkhawaga  std::ios_base::sync_with_stdio(0)
+using namespace std;
+ 
+bool cmp(pair<string, ll>& a, pair<string, ll>& b)
+{
+    return  a.second == b.second ? a.first<b.first : a.second>b.second;
+}
+ 
+ 
+//vector<ll> getDivisors(int n)
+//{
+//    vector<ll>vec;
+//    // Note that this loop runs till square root
+//    for (int i = 1; i <= sqrt(n); i++)
+//    {
+//        if (n % i == 0)
+//        {
+//            // If divisors are equal, print only one
+//            if (n / i == i)
+//                vec.push_back(i);
+//
+//            else // Otherwise print both
+//            {
+//                vec.push_back(i);
+//                vec.push_back( n / i);
+//            }
+//        }
+//    }
+//    return vec;
+//}
+ 
+void tcase()
+{
+    ll a, b, c, n;
+    cin >> a >> b >> c >> n;
+    if (a >= b && a >= c)
+    {
+        n -= (a - b);
+        n -= (a - c);
+    }
+    else if (b >= a && b >= c)
+    {
+        n -= (b - a);
+        n -= (b - c);
+    }
+    else
+    {
+        n -= (c - b);
+        n -= (c - a);
+    }
+    if ( n >= 0&&n % 3 == 0) cout << "YES\n";
+    else cout << "NO\n";
+}
+int main()
+{
+    elkhawaga;
+    ll t;
+    cin >> t;
+    //t = 1;
+    for (int i = 0;i < t;i++) tcase();
+}
