b6a4c22d52eef4a30d97e0f969431f1242e6ab48
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-02-09 22:41:44 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1816A | Ian Visits Mary/202818180.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+ ll t;cin >> t;
+ while (t--)
+ {
+  ll x, y;cin >> x >> y;
+  cout << 2<<"\n";
+  cout << x - 1 << " " << 1<<"\n";
+  cout << x << " " << y<<"\n";
+ }
+}
