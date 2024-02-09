528c2aa55e8b2b214b84c42d000e7745a9eb1887
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-02-09 22:43:25 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1791A | Codeforces Checking/191866868.cpp
@@ -0,0 +1,20 @@
+#include "bits/stdc++.h"
+using namespace std;
+int main()
+{
+ int n;
+ cin >> n;
+ while (n--)
+ {
+  char x;
+  cin >> x;
+  if (x == 'c' || x == 'o' || x == 'd' || x == 'e' || x == 'f' || x == 'o' || x == 'r' || x == 'e' || x == 's') cout << "YES\n";
+  else cout << "NO\n";
+ 
+ }
+ return 0;
+}
+ 
+ 
+ 
+ 
