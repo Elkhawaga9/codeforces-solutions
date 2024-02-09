19611366b7b3166692940fcdef89ab01871401d9
Ahmed Salah <108738651+Elkhawaga9@users.noreply.github.com>
2024-02-09 22:41:27 +0200

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1829B | Blank Space/204825158.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define elkhawaga ios_base::sync_with_stdio(false);cin.tie(nullptr)
+#define pi 3.14159265358979323846
+typedef long long ll;
+typedef long double ld;
+const ll N = 10e9 + 1;
+ll po[32];
+string pei = "3141592653589793238462643383279";
+void tcase()
+{
+ 
+    ll n;cin >> n;
+    vector<int>vec;
+    ll ans1=0, ans2=0;
+    while (n--)
+    {
+        int temp;cin >> temp;
+        if (temp == 0)
+        {
+            ans1++;
+        }
+        else
+        {
+            ans2 = max(ans1, ans2);
+            ans1 = 0;
+        }
+    }
+    cout << max(ans1,ans2) << "\n";
+ 
+}
+int main()
+{
+    elkhawaga;
+    ll t;cin >> t;
+    while(t--)
+    tcase();
+    return 0;
+}
